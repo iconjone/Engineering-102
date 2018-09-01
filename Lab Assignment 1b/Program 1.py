538b5869c50f05bf54d5c57c51afe488a86d45f3
Jonathan Davis Samuel <jone.bala@gmail.com>
2018-08-31 23:22:18 -0500

    init

---
+++ Lab Assignment 1b/Program 1.py
@@ -0,0 +1,63 @@
+# By submitting this assignment, I agree to the following:
+#  “Aggies do not lie, cheat, or steal, or tolerate those who do”
+#  “I have not given or received any unauthorized aid on this assignment”
+#
+# Name: 		Jonathan Samuel
+# Section:		510
+# Assignment:	Lab 1
+# Date:		    30 08 2018
+import math
+# Question A
+print ("Jonathan Samuel UIN:" + str(427005068) + " Section Number:"+ str(510))
+#Question B
+print("I play the piano")
+#Question C
+r=20
+i=5
+v=i*r
+print(v)
+#Question D
+m=100
+ve=21
+ke=.5*100*(math.pow(ve,2))
+print(ke)
+#Question E
+ver = 100
+kvi = 1.2
+ldi = 2.5
+rn = (ver*ldi)/(kvi)
+print(rn)
+#Question F
+t=2200
+sbc = 5.67 * math.pow(10,-8)
+sb = sbc*math.pow(t,4)
+print(sb)
+#Question G
+d = 20
+ipr = 100
+dr = 2
+hc =.8
+p = ipr * math.pow(1+(hc*(dr)*d),-1/hc )
+print(p)
+#Question H
+lam = 20
+mu = 35
+p = lam/mu
+a = p/(1-p)
+print(a)
+#Question I
+s=20
+co=2
+ang=35
+t = s*math.tan(ang) + co
+print(t)
+#Question J
+wl = 7.5 * math.pow(10 , -7)
+d = 1 * math.pow(10, -6)
+ang = math.degrees(math.asin(wl/(2*d)))
+print(ang)
+
+
+
+
+
